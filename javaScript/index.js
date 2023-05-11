0b51660039099520c6773b7220ee1b3881607de5
DebSofia <deb.sofiasilva@gmail.com>
2023-05-11 12:31:40 +0100

    set the default timezone to user's location

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,6 +1,6 @@
 const TIME_FORMAT = "h:mm:ss A";
 const DATE_FORMAT = "dddd, DD MMM YYYY";
-const DEFAULT_TIMEZONE = "Europe/Lisbon";
+const DEFAULT_TIMEZONE = moment.tz.guess();
 
 let cityNameElement = document.querySelector("#city-name");
 let dateElement = document.querySelector("#date");
@@ -10,19 +10,25 @@ let timeZone = DEFAULT_TIMEZONE;
 dateElement.innerHTML = moment().format(DATE_FORMAT);
 timeElement.innerHTML = moment.tz(DEFAULT_TIMEZONE).format(TIME_FORMAT);
 
+changeCityAndDateTime(timeZone);
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
+  changeCityAndDateTime(cityTimeZone);
+
+  timeZone = cityTimeZone;
+}
+
+function changeCityAndDateTime(cityTimeZone) {
   let cityTime = moment().tz(cityTimeZone);
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 
   cityNameElement.innerHTML = cityName;
   dateElement.innerHTML = cityTime.format(DATE_FORMAT);
   timeElement.innerHTML = cityTime.format(TIME_FORMAT);
-
-  timeZone = cityTimeZone;
 }
 
 function updateTime(params) {
