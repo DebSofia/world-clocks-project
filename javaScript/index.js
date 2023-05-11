343c7c89fc611002954ceabe2982f67136d10030
DebSofia <deb.sofiasilva@gmail.com>
2023-05-11 12:06:47 +0100

    Added JavaScript in order to show current time and date when selecting a city and added the set time interval to display the seconds changing. Removed the text align center in CSS.

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,6 +1,33 @@
+const TIME_FORMAT = "h:mm:ss A";
+const DATE_FORMAT = "dddd, DD MMM YYYY";
+const DEFAULT_TIMEZONE = "Europe/Lisbon";
+
 let cityNameElement = document.querySelector("#city-name");
 let dateElement = document.querySelector("#date");
 let timeElement = document.querySelector("#time");
+let timeZone = DEFAULT_TIMEZONE;
 
-dateElement.innerHTML = moment().format("dddd, DD MMM YYYY");
-timeElement.innerHTML = moment.tz("Europe/Lisbon").format("h:mm A");
+dateElement.innerHTML = moment().format(DATE_FORMAT);
+timeElement.innerHTML = moment.tz(DEFAULT_TIMEZONE).format(TIME_FORMAT);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
+  cityNameElement.innerHTML = cityName;
+  dateElement.innerHTML = cityTime.format(DATE_FORMAT);
+  timeElement.innerHTML = cityTime.format(TIME_FORMAT);
+
+  timeZone = cityTimeZone;
+}
+
+function updateTime(params) {
+  timeElement.innerHTML = moment.tz(timeZone).format(TIME_FORMAT);
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#cities");
+citiesSelectElement.addEventListener("change", updateCity);
